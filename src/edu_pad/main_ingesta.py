a1b08202ea310df2bdf894e9cdf1226309b82dcf
j-benit <146685640+j-benit@users.noreply.github.com>
2025-06-07 23:22:40 +0000

    dframe

--- src/edu_pad/main_ingesta.py
+++ src/edu_pad/main_ingesta.py
@@ -4,7 +4,7 @@ from edu_pad.database import DataBase
 import pandas as pd
 
 def main():
-    ruta_csv = "/workspaces/PAD_2025_1/proyecto_integrado_iii/src/edu_pad/static/csv/productos_mercadolibre_limpio.csv"
+    ruta_csv = "/workspaces/PAD_2025_1/src/edu_pad/static/csv/productos_mercadolibre_limpio.csv"
     columnas_esperadas = ["Marca", "Título", "Precio Anterior", "Precio Actual", "Descuento", "Cuotas", "Calificación", "Reseñas", "URL", "Promocionado"]
 
     database = DataBase(ruta_csv, columnas_esperadas)
