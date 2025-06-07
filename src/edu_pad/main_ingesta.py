a3d8ba745b5fbfe348251c5f47337f7bb8bd2964
j-benit <146685640+j-benit@users.noreply.github.com>
2025-06-07 23:38:44 +0000

    yml y main_ingesta

--- src/edu_pad/main_ingesta.py
+++ src/edu_pad/main_ingesta.py
@@ -1,15 +1,16 @@
-
 from edu_pad.database import DataBase
-
 import pandas as pd
+import os
 
 def main():
-    ruta_csv = "/workspaces/PAD_2025_1/src/edu_pad/static/csv/productos_mercadolibre_limpio.csv"
+    ruta_csv = "/app/static/csv/productos_mercadolibre_limpio.csv"
     columnas_esperadas = ["Marca", "Título", "Precio Anterior", "Precio Actual", "Descuento", "Cuotas", "Calificación", "Reseñas", "URL", "Promocionado"]
+
+    os.makedirs(os.path.dirname(ruta_csv), exist_ok=True)
 
     database = DataBase(ruta_csv, columnas_esperadas)
     df = database.cargar_df()
-    # Aquí puedes procesar df si quieres
     database.guardar_df(df)
+
 if __name__ == "__main__":
     main()
